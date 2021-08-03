8733ef04e3f82957ace59fc8fc4d2059a160d1ea
Paulo Costa <hello@pcosta.eu>
2021-08-03 14:48:21 +0100

    Update FlickerApiService.kt

--- app/src/main/java/eu/pcosta/flickergallery/api/FlickerApiService.kt
+++ app/src/main/java/eu/pcosta/flickergallery/api/FlickerApiService.kt
@@ -15,7 +15,7 @@ import retrofit2.http.Query
  * Flicker API Methods
  */
 interface FlickerApi {
-    @GET("?method=flickr.photos.search&api_key=9a95c68a9c6ec61104cd3967dcbb8bd3&format=json&nojsoncallback=1")
+    @GET("?method=flickr.photos.search&format=json&nojsoncallback=1")
     fun getPhotos(
         @Query("api_key") apiKey: String,
         @Query("tags") tags: String,
